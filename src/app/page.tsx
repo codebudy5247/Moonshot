7e7e701d37020aa8bfcb99690066050f57a9e659
codebudy5247 <a.shekhar5247@gmail.com>
2024-10-18 18:49:17 +0530

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col gap-4">
+        <a
+          className="text-3xl font-semibold text-blue-700 underline"
+          href={`https://${"moonshot-task.vercel.app/email-dashboard"}`}
+          target="_blank"
+          rel={"noreferrer"}
+        >
+          Q1:Email {"{Solution}"}
+        </a>
+        <a
+          className="text-3xl font-semibold text-blue-700 underline"
+          href={`https://${"moonshot-task.vercel.app/analytics-dashboard"}`}
+          target="_blank"
+          rel={"noreferrer"}
+        >
+          Q2:Visualization {"{Solution}"}
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
