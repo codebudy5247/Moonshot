f65939412f7dedc87cdd9a1df443fd598df2834c
codebudy5247 <a.shekhar5247@gmail.com>
2024-10-18 18:58:12 +0530

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,26 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex flex-col gap-5 justify-center items-center">
+      <h1 className="text-4xl font-bold">
+        Moonshot:Fullstack-Test-2024-10-12 {"{Solution}"}
+      </h1>
       <div className="flex flex-col gap-4">
         <a
-          className="text-3xl font-semibold text-blue-700 underline"
+          className="text-2xl font-semibold text-blue-700 underline"
           href={`https://${"moonshot-task.vercel.app/email-dashboard"}`}
           target="_blank"
           rel={"noreferrer"}
         >
-          Q1:Email {"{Solution}"}
+          Q1:Email
         </a>
         <a
-          className="text-3xl font-semibold text-blue-700 underline"
+          className="text-2xl font-semibold text-blue-700 underline"
           href={`https://${"moonshot-task.vercel.app/analytics-dashboard"}`}
           target="_blank"
           rel={"noreferrer"}
         >
-          Q2:Visualization {"{Solution}"}
+          Q2:Visualization
         </a>
       </div>
     </div>
